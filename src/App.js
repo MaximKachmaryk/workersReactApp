136f230b81ddf5acc5667a9852bafb4750a1c7fd
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-08 19:55:35 +0300

    add state

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import SearchPanel from "./components/search-panel/search-panel";
 
 function App() {
   const data = [
-    { name: "John C.", salary: 900, increase: false },
-    { name: "Alex M.", salary: 3000, increase: true },
-    { name: "Carl W.", salary: 5000, increase: false },
+    { name: "John C.", salary: 900, increase: false, id :1},
+    { name: "Alex M.", salary: 3000, increase: true , id :2},
+    { name: "Carl W.", salary: 5000, increase: false ,id :3 }
   ];
 
   return (
